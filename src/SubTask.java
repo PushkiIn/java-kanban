c6d9afccf6f029290a81b24db1aabf57b9371b02
suguha <obivanyakenobi@gmail.com>
2025-02-03 23:15:45 +0300

    Создан класс SubTask

---
+++ src/SubTask.java
@@ -0,0 +1,25 @@
+public class SubTask extends Task {
+    private int epicId;
+
+    public SubTask(String title, String description, int epicId) {
+        super(title, description);
+        this.epicId = epicId;
+    }
+
+    public void setEpicId(int epicId) {
+        this.epicId = epicId;
+    }
+
+    public int getEpicId() {
+        return epicId;
+    }
+
+    @Override
+    public String toString() {
+        return "SubTask{id=" + getId() +
+                ", name='" + getName() + '\'' +
+                ", description='" + getDescription() + '\'' +
+                ", status=" + getStatus() +
+                ", epicId=" + epicId + '}';
+    }
+}
