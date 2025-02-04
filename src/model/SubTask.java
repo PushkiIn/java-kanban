f0b8e3f1602612149c0199776b159df5929a8bd0
suguha <obivanyakenobi@gmail.com>
2025-02-04 22:06:37 +0300

    Добавлен конструктор с параметрами для установки статуса.

--- src/model/SubTask.java
+++ src/model/SubTask.java
@@ -1,8 +1,17 @@
+package model;
+
+import enums.Status;
+
 public class SubTask extends Task {
     private int epicId;
 
     public SubTask(String title, String description, int epicId) {
         super(title, description);
+        this.epicId = epicId;
+    }
+
+    public SubTask(String title, String description, Status status, int epicId) {
+        super(title, description, status);
         this.epicId = epicId;
     }
 
@@ -16,7 +25,7 @@ public class SubTask extends Task {
 
     @Override
     public String toString() {
-        return "SubTask{id=" + getId() +
+        return "model.SubTask{id=" + getId() +
                 ", name='" + getName() + '\'' +
                 ", description='" + getDescription() + '\'' +
                 ", status=" + getStatus() +
