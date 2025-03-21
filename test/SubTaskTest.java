5d5a1ad71d5a33443471d3a8805bb73100b6d142
suguha <obivanyakenobi@gmail.com>
2025-03-21 03:59:04 +0300

    Refactor: тесты перенесены из .src/test в ./test

---
+++ test/SubTaskTest.java
@@ -0,0 +1,22 @@
+package test;
+
+import model.SubTask;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SubTaskTest {
+    @Test
+    public void testSubTask() {
+        SubTask subTask = new SubTask("Подзадача 1", "Описание подзадачи 1", 1);
+        Assertions.assertEquals("Подзадача 1", subTask.getName());
+        Assertions.assertEquals("Описание подзадачи 1", subTask.getDescription());
+        Assertions.assertEquals(1, subTask.getEpicId());
+    }
+
+    @Test
+    public void testSetAndGetEpicId() {
+        SubTask subTask = new SubTask("Подзадача 1", "Описание подзадачи 1", 1);
+        subTask.setEpicId(5);
+        Assertions.assertEquals(5, subTask.getEpicId());
+    }
+}
