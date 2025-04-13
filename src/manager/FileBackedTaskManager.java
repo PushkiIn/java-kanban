c88d550cb8ba9a63b4d0d95ee7b5add236542548
suguha <obivanyakenobi@gmail.com>
2025-04-13 12:00:47 +0300

    Метод FromString перенесен в класс StringConverter; Изменения для метода loadFromString для избежания конверсии id; Метод toString() теперь используется из реализации в классах Task/Subtask/Epic

--- src/manager/FileBackedTaskManager.java
+++ src/manager/FileBackedTaskManager.java
@@ -1,10 +1,9 @@
 package manager;
 
-import enums.Status;
-import enums.TaskType;
 import model.Epic;
 import model.SubTask;
 import model.Task;
+import tools.StringConverter;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -82,6 +81,7 @@ public class FileBackedTaskManager extends InMemoryTaskManager implements TaskMa
 
     public static FileBackedTaskManager loadFromFile(File file) {
         FileBackedTaskManager manager = new FileBackedTaskManager();
+        int maxId = 0;
 
         try (BufferedReader br = new BufferedReader(new FileReader(file))) {
             String line = br.readLine();
@@ -89,7 +89,8 @@ public class FileBackedTaskManager extends InMemoryTaskManager implements TaskMa
             while ((line = br.readLine()) != null) {
                 if (line.isBlank()) continue;
 
-                Task task = fromString(line);
+                Task task = StringConverter.fromString(line);
+                if (task.getId() > maxId) maxId = task.getId();
 
                 int id = task.getId();
                 switch (task.getType()) {
@@ -109,70 +110,32 @@ public class FileBackedTaskManager extends InMemoryTaskManager implements TaskMa
             e.printStackTrace();
         }
 
+        manager.counterId = maxId + 1;
+
         return manager;
     }
 
     public void save() {
-        try (FileWriter writer = new FileWriter("tasks.csv")) {
-            writer.write("id,type,name,status,description,epic\n");
+        String fileName = "tasks.csv";
+        String firstString = "id,type,name,status,description,epic\n";
+
+        try (FileWriter writer = new FileWriter(fileName)) {
+            writer.write(firstString);
 
             for (Task task : super.getAllTasks()) {
-                writer.write(toString(task) + "\n");
+                writer.write(task.toString() + "\n");
             }
 
             for (Epic epic : super.getAllEpics()) {
-                writer.write(toString(epic) + "\n");
+                writer.write(epic.toString() + "\n");
             }
 
             for (SubTask subTask : super.getAllSubTasks()) {
-                writer.write(toString(subTask) + "\n");
+                writer.write(subTask.toString() + "\n");
             }
 
         } catch (IOException e) {
             System.out.println("Ошибка при записи в файл");
-        }
-    }
-
-    public static Task fromString(String value) {
-        String[] fields = value.split(",");
-        int id = Integer.parseInt(fields[0]);
-        TaskType type = TaskType.valueOf(fields[1]);
-        String name = fields[2];
-        Status status = Status.valueOf(fields[3]);
-        String description = fields[4];
-        int epicId = fields.length > 5 ? Integer.parseInt(fields[5]) : 0;
-
-        switch (type) {
-            case TASK:
-                return new Task(id, name, description, status);
-            case EPIC:
-                return new Epic(id, name, description, status);
-            case SUBTASK:
-                return new SubTask(id, name, description, status, epicId);
-            default:
-                throw new IllegalArgumentException("Unknown task type: " + type);
-        }
-    }
-
-    private String toString(Task task) {
-        if (task instanceof SubTask) {
-            SubTask subTask = (SubTask) task;
-            return String.format("%d,%s,%s,%s,%s,%d",
-                    subTask.getId(),
-                    subTask.getType(),
-                    subTask.getName(),
-                    subTask.getStatus(),
-                    subTask.getDescription(),
-                    subTask.getEpicId()
-            );
-        } else {
-            return String.format("%d,%s,%s,%s,%s",
-                    task.getId(),
-                    task.getType(),
-                    task.getName(),
-                    task.getStatus(),
-                    task.getDescription()
-            );
         }
     }
 }
