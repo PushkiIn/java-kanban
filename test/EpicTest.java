246ca73e985d9f175b704c7cee8a4cf27e89b8ae
madoka <obivanyakenobi@gmail.com>
2025-07-11 07:55:57 +0300

    refactor: renamed SubTask to Subtask and updated references

--- test/EpicTest.java
+++ test/EpicTest.java
@@ -1,5 +1,5 @@
 import model.Epic;
-import model.SubTask;
+import model.Subtask;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -16,32 +16,32 @@ class EpicTest {
     @Test
     public void testGetSubTasksIds() {
         Epic epic = new Epic("Эпик 1", "Описание эпика 1");
-        SubTask subTask1 = new SubTask("Подзадача 1", "Описание подзадачи 1", epic.getId());
-        SubTask subTask2 = new SubTask("Подзадача 2", "Описание подзадачи 2", epic.getId());
-        subTask2.setId(5);
-        epic.addSubTaskId(subTask1.getId());
-        epic.addSubTaskId(subTask2.getId());
+        Subtask subtask1 = new Subtask("Подзадача 1", "Описание подзадачи 1", epic.getId());
+        Subtask subtask2 = new Subtask("Подзадача 2", "Описание подзадачи 2", epic.getId());
+        subtask2.setId(5);
+        epic.addSubTaskById(subtask1.getId());
+        epic.addSubTaskById(subtask2.getId());
         ArrayList<Integer> expectedSubTaskIds = new ArrayList<>();
-        expectedSubTaskIds.add(subTask1.getId());
-        expectedSubTaskIds.add(subTask2.getId());
-        Assertions.assertEquals(expectedSubTaskIds, epic.getSubTaskIds());
+        expectedSubTaskIds.add(subtask1.getId());
+        expectedSubTaskIds.add(subtask2.getId());
+        Assertions.assertEquals(expectedSubTaskIds, epic.getSubTasks());
     }
 
     @Test
     public void addSubTasksId() {
         Epic epic = new Epic("Эпик 1", "Описание эпика 1");
-        SubTask subTask = new SubTask("Подзадача 1", "Описание подзадачи 1", epic.getId());
-        epic.addSubTaskId(subTask.getId());
-        Assertions.assertTrue(epic.getSubTaskIds().contains(subTask.getId()));
+        Subtask subTask = new Subtask("Подзадача 1", "Описание подзадачи 1", epic.getId());
+        epic.addSubTaskById(subTask.getId());
+        Assertions.assertTrue(epic.getSubTasks().contains(subTask.getId()));
     }
 
     @Test
     public void testRemoveSubTaskId() {
         Epic epic = new Epic("Эпик 1", "Описание эпика 1");
-        SubTask subTask = new SubTask("Подзадача 1", "Описание подзадачи 1", epic.getId());
-        epic.addSubTaskId(subTask.getId());
-        Assertions.assertTrue(epic.getSubTaskIds().contains(subTask.getId()));
-        epic.removeSubTaskId(subTask.getId());
-        Assertions.assertFalse(epic.getSubTaskIds().contains(subTask.getId()));
+        Subtask subTask = new Subtask("Подзадача 1", "Описание подзадачи 1", epic.getId());
+        epic.addSubTaskById(subTask.getId());
+        Assertions.assertTrue(epic.getSubTasks().contains(subTask.getId()));
+        epic.removeSubTaskById(subTask.getId());
+        Assertions.assertFalse(epic.getSubTasks().contains(subTask.getId()));
     }
 }
