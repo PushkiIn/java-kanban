35044cebdee02e7fa356f503de0f4599f688be9a
madoka <obivanyakenobi@gmail.com>
2025-07-11 07:19:10 +0300

    feat(json): add DurationAdapter for custom Duration serialization

---
+++ src/util/DurationAdapter.java
@@ -0,0 +1,25 @@
+package util;
+
+import com.google.gson.TypeAdapter;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
+
+import java.io.IOException;
+import java.time.Duration;
+
+public class DurationAdapter extends TypeAdapter<Duration> {
+    @Override
+    public void write(JsonWriter out, Duration value) throws IOException {
+        if (value != null) {
+            out.value(value.toString()); // формат PT1H2M3S и т.п.
+        } else {
+            out.nullValue();
+        }
+    }
+
+    @Override
+    public Duration read(JsonReader in) throws IOException {
+        String durationString = in.nextString();
+        return Duration.parse(durationString);
+    }
+}
